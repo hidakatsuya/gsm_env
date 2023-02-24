db6f7155cc925f70b20c3d6cab3b4a0947ba74a1
Katsuya Hidaka <hidakatsuya@gmail.com>
2023-02-25 03:08:50 +0900

    specify the expectations of the test

--- test/gsm_env/secret_test.rb
+++ test/gsm_env/secret_test.rb
@@ -6,13 +6,20 @@ class GsmEnv::SecretTest < Test::Unit::TestCase
   include GsmHelper
 
   test 'accessors' do
-    g_secret = gsm_secret
-    g_secret_version = gsm_secret_version
+    secret_path = { project_id: '123', secret_id: 'ABC' }
+
+    g_secret = gsm_secret(
+      name: gsm_secret_path(**secret_path)
+    )
+    g_secret_version = gsm_secret_version(
+      name: gsm_secret_version_path(**secret_path, version: 9),
+      data: 'DATA'
+    )
 
     secret = GsmEnv::Secret.new(g_secret, g_secret_version)
 
-    assert_equal 'TEST_ENV', secret.name
-    assert_equal 'abc123', secret.value
+    assert_equal 'ABC', secret.name
+    assert_equal 'DATA', secret.value
     assert_equal g_secret, secret.secret
     assert_equal g_secret_version, secret.version
   end
