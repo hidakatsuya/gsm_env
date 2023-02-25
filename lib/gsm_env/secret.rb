8919e32eec9efa3ac98db54d7d875e3014672f7e
Katsuya Hidaka <hidakatsuya@gmail.com>
2023-02-26 03:56:07 +0900

    obtain secret name strictly from resource name

--- lib/gsm_env/secret.rb
+++ lib/gsm_env/secret.rb
@@ -1,13 +1,34 @@
 # frozen_string_literal: true
 
 module GsmEnv
-  Secret = Struct.new(:secret, :version) do
+  class Secret
+    class InvalidName < StandardError; end
+
+    attr_reader :secret, :version
+
+    def initialize(secret, version)
+      @secret = secret
+      @version = version
+    end
+
     def name
-      @name ||= secret.name.split('/').fetch(3)
+      @name ||= extract_secret_name_from(secret.name)
     end
 
     def value
       version.payload.data&.chomp
+    end
+
+    private
+
+    SECRET_RESOURCE_NAME_REGEXP = %r{^projects/.+/secrets/(?<secret>[^/]+)$}
+
+    def extract_secret_name_from(secret_resource_name)
+      match = SECRET_RESOURCE_NAME_REGEXP.match(secret_resource_name)
+
+      raise InvalidName unless match && match['secret']
+
+      match['secret']
     end
   end
 end
